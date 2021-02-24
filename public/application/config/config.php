3eb45217c204c9882835151b91924b1eff363d39
Henrique Martins <hmartins224@gmail.com>
2021-02-24 17:02:04 +0000

    Configuring the home page

--- public/application/config/config.php
+++ public/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'https://8001-amaranth-opossum-pb8y296x.ws-us03.gitpod.io/';
 
 /*
 |--------------------------------------------------------------------------
@@ -324,7 +324,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'gQWaXFlVgn3r3Rhh9eLkYBz6noiTNwak';
 
 /*
 |--------------------------------------------------------------------------
---
+++ public/application/views/home.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="utf-8">
+	<title>Home</title>
+</head>
+<body>
+	<h1>Bem vindo</h1>
+</body>
+</html>
